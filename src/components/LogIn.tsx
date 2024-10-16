816110c2782ebc105dab3f3b3bf0d26d2c0a0f36
Ketharagan <e20199@eng.pdn.ac.lk>
2024-10-16 09:37:53 +0530

    Chang the view of user log in page

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -3,6 +3,9 @@ import logo from '../assets/logo.png';
 import { signin } from '../services/AuthService';
 import { AxiosError } from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { HiUser, HiLockClosed } from 'react-icons/hi';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { useState } from 'react';
 
 const LoginIn = () => {
   const navigate = useNavigate();
@@ -11,9 +14,11 @@ const LoginIn = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
   const onSignIn = async (data: FieldValues) => {
     try {
-      const response = await signin({
+      await signin({
         userName: data.username,
         password: data.password,
       });
@@ -22,86 +27,132 @@ const LoginIn = () => {
     } catch (error) {
       if (error instanceof AxiosError) {
         if (!error.response) {
-          console.error('Network error or server not responding');
-          alert(
+          setErrorMessage(
             'Network error or server not responding. Please try again later.'
           );
         } else if (error.response?.status === 400) {
-          console.error('Invalid credentials:', error.response.data);
-          alert('Invalid username or password. Please try again.');
+          setErrorMessage('Invalid username or password. Please try again.');
         } else if (error.response?.status === 500) {
-          console.error('Server error:', error.response.data);
-          alert('An internal server error occurred. Please try again later.');
+          setErrorMessage(
+            'An internal server error occurred. Please try again later.'
+          );
+        } else {
+          const message = error.response?.data.message || error.message;
+          console.log('Error in login: ', message);
+          setErrorMessage(message);
         }
+      } else {
+        // Handle other unexpected errors
+        setErrorMessage('An unexpected error occurred.');
+        console.error('Unexpected error:', error);
       }
     }
   };
 
-  //   console.log(localStorage.getItem('userType'));
-  //   console.log(localStorage.getItem('token'));
-
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-300">
-      <div className="sm:w-2/3 w-full h-auto justify-center p-7 shadow-xl rounded-md bg-white">
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <div className="flex h-screen bg-blue-200 px-32 py-20">
+      {/* Login page Description */}
+      <div className="w-1/2 hidden md:flex flex-col justify-center items-center text-white bg-gradient-to-r from-violet-600 to-blue-500 p-8 mt-14 rounded-l-3xl">
+        <img alt="FoE-Room Reservation" src={logo} className="h-16 mb-6" />
+        <h1 className="text-4xl font-bold mb-4">
+          Welcome to FoE Room Reservation
+        </h1>
+        <p className="text-lg leading-relaxed text-center">
+          A platform to efficiently book and manage rooms at the Faculty of
+          Engineering, University of Peradeniya. Log in to create new bookings,
+          manage existing reservations, and view room availability.
+        </p>
+      </div>
+
+      {/* User login Form */}
+      <div className="w-full md:w-1/2 flex justify-center items-center bg-gray-100 mt-14 rounded-r-3xl">
+        <div className="w-full max-w-md px-8 py-12 shadow-xl rounded-lg bg-white">
+          <div className="flex flex-col items-center">
             <img
               alt="FoE-Room Reservation"
               src={logo}
-              className="mx-auto h-10 w-auto"
+              className="h-12 mb-4 md:hidden"
             />
+            <h2 className="text-center text-3xl font-bold mb-6 text-gray-800">
+              Sign in to your account
+            </h2>
           </div>
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form onSubmit={handleSubmit(onSignIn)} className="space-y-6">
-              {/* User Name */}
-              <div>
-                <label
-                  htmlFor="username"
-                  className="block text-base font-medium leading-6 text-gray-900">
-                  Username
-                </label>
+          <form onSubmit={handleSubmit(onSignIn)} className="space-y-6">
+            {/* User Name */}
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-lg font-medium text-gray-700">
+                Username
+              </label>
+              <div className="relative mt-2">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <HiUser className="text-gray-400" />
+                </span>
                 <input
                   {...register('username', { required: true })}
                   id="username"
                   type="text"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"
+                  className="block w-full pl-10 pr-4 rounded-md border border-gray-300 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
                   placeholder="Enter your username"
+                  onClick={() => setErrorMessage(null)}
                 />
-                {errors.username?.type === 'required' && (
-                  <p className="text-red-600">Username field is required</p>
-                )}
               </div>
+              {errors.username?.type === 'required' && (
+                <p className="text-red-600 mt-2">Username field is required</p>
+              )}
+            </div>
 
-              {/* Password */}
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-base font-medium leading-6 text-gray-900">
-                  Password
-                </label>
+            {/* Password */}
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-lg font-medium text-gray-700">
+                Password
+              </label>
+              <div className="relative mt-2">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <HiLockClosed className="text-gray-400" />
+                </span>
                 <input
                   {...register('password', { required: true })}
                   id="password"
                   type="password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"
+                  className="block w-full pl-10 pr-4 rounded-md border border-gray-300 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
                   placeholder="Enter your password"
+                  onClick={() => setErrorMessage(null)}
                 />
-                {errors.password?.type === 'required' && (
-                  <p className="text-red-600">Password field is required</p>
-                )}
               </div>
+              {errors.password?.type === 'required' && (
+                <p className="text-red-600 mt-2">Password field is required</p>
+              )}
+            </div>
 
-              {/* Sign in Button */}
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                  Sign in
-                </button>
+            {errorMessage && (
+              <div className="p-2 bg-red-600 text-gray-100 hover:bg-red-500 rounded-md flex items-start space-x-3 shadow-sm">
+                <div
+                  className="flex-shrink-0 text-white transition-transform transform hover:scale-110 duration-100"
+                  onClick={() => setErrorMessage(null)}>
+                  {/* Error Icon */}
+                  <HighlightOffIcon />
+                </div>
+                <div className="flex-1">
+                  <p className="font-medium text-white">Error</p>
+                  <p>{errorMessage}</p>
+                </div>
               </div>
-            </form>
-          </div>
+            )}
+
+            {/* Sign in Button */}
+            <div>
+              <button
+                type="submit"
+                className="w-full bg-indigo-600 text-white py-2 rounded-md shadow-md hover:bg-indigo-500 focus:ring-2 focus:ring-indigo-500">
+                Sign in
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
