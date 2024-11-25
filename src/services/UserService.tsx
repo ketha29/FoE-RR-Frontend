ff433e24d943c27148f9fe2dfe673538a8564299
Ketharagan Parameswaran <150885176+ketha29@users.noreply.github.com>
2024-11-25 14:18:34 +0530

    Send token to backend for getAllUsers

--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -1,14 +1,23 @@
 import axios from 'axios';
+import { isAuthenticated } from './AuthService';
 
 // Base URL for the API
 const BASE_URL = 'http://localhost:8082/user';
+
+// token
+const token = isAuthenticated() ? localStorage.getItem('token') : null;
 
 // Endpoints
 const GET_USERS = `${BASE_URL}/all`;
 const ADD_USER = `${BASE_URL}/add-user`;
 const DELETE_USER = (userId: number) => `${BASE_URL}/delete-user/${userId}`;
 
-export const getAllUsers = () => axios.get(GET_USERS);
+export const getAllUsers = () =>
+  axios.get(GET_USERS, {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  });
 
 export const addUser = (user: any) => {
   return axios.post(ADD_USER, user, {
